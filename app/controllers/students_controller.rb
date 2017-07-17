1f42e516dbaffb8fae59e8d7a188cd88b616cad3
anshul <anshulgupta0895@gmail.com>
2017-07-17 15:22:42 +0530

    fourth commit

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,23 +7,42 @@ class StudentsController < ApplicationController
 
   def index
     if Admission.current_user == true
-      @students = Admission.all
+      @admissions = Admission.all.order(:created_at)
     else
       redirect_to :controller=> "welcome", :action=> "index"
     end
   end
 
   def create
-    @admin = Admission.new(name: params[:student][:name], dob: params[:student][:dob], city: params[:student][:city], reg_no: params[:student][:regno])
-    @admin.save
+    @admission = Admission.new(name: params[:student][:name], dob: params[:student][:dob], city: params[:student][:city], reg_no: params[:student][:regno])
+    @admission.save
     redirect_to action: "new"
   end
 
   def show
     if Admission.current_user == true
-      @admin = Admission.find(params[:id])
+      @admission = Admission.find(params[:id])
     else
       redirect_to :controller=> "welcome", :action=> "index"
+    end
+  end
+
+  def destroy
+    @admission = Admission.find(params[:id])
+    @admission.destroy
+    redirect_to students_path
+  end
+
+  def edit
+    @admission = Admission.find(params[:id])
+  end
+
+  def update
+    @admission = Admission.find(params[:id])
+    if @admission.update(name: params[:admission][:name], dob: params[:admission][:dob], city: params[:admission][:city], reg_no: params[:admission][:reg_no])
+      redirect_to action: "index"
+    else
+      render 'edit'
     end
   end
 
--- app/models/admission.rb
+++ app/models/admission.rb
@@ -1,4 +1,9 @@
 class Admission < ApplicationRecord
+  validates :name, presence: true, length: { minimum: 3 }
+  # validates :dob, :presence => {message: "dob must be filled"}
+  # validates :city, :presence => {message: "city must be filled"}
+  # validates :reg_no, :presence => {message: "reg_no must be filled"}
+  # errors.add(:name, "cannot contain the characters !@#%*()_-+=")
   @@current_user = false
   def self.current_user
     @@current_user
